021c078c7f9fe2770ae256849a5aaecdef166187
dschoi <dschoi@aircode.com>
2021-03-08 10:05:03 +0900

    1

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 const pool = mysql.createPool({
-    connectionLimit: 10,
+    connectionLimit: 10,    
     host: 'mysql',
     user: 'root',
     password: 'dongseon',
